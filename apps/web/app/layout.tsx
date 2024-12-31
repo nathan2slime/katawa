cf617b7455d7ea81109129758962a41a7ef358f6
Jhonathan M. <nathan3boss@gmail.com>
2024-12-31 12:36:35 -0300

    feat(web): finish signing page (#2)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,23 +1,26 @@
 import type { Metadata } from 'next'
-import { Baloo_2 } from 'next/font/google'
+import { Baloo_2, Titillium_Web } from 'next/font/google'
 import { cn } from '~/lib/utils'
 import type { AppChildren } from '~/types'
 
 import { Providers } from '~/components/providers'
 
 import '~/app/globals.css'
+import { AuthGuard } from '~/components/auth-guard'
 
 export const metadata: Metadata = {}
 
-const baloo = Baloo_2({
+const baloo = Titillium_Web({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800']
+  weight: ['400', '300', '600', '700', '900', ]
 })
 
 const RootLayout = ({ children }: Readonly<AppChildren>) => (
   <html lang="en">
     <body className={cn(baloo.className)}>
-      <Providers>{children}</Providers>
+      <Providers>
+        <AuthGuard>{children}</AuthGuard>
+      </Providers>
     </body>
   </html>
 )
