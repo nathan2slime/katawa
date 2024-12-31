0e2c6218f825c46dd6d804c674e7511144cea29a
Jhonathan <nathan3boss@gmail.com>
2024-12-31 13:14:36 -0300

    feat(seed): create seed for user admin

--- packages/seed/logger.mjs
+++ packages/seed/logger.mjs
@@ -1,5 +1,14 @@
 import { createLogger } from 'bunyan'
 
 export const logger = createLogger({
-  name: '@kwa/seed'
+  name: '@kwa/seed',
+  streams: [
+    {
+      level: 'info',
+      stream: process.stdout
+    },
+    {
+      path: 'debug.log'
+    }
+  ]
 })
--- packages/seed/manager.mjs
+++ packages/seed/manager.mjs
@@ -4,23 +4,37 @@ import { faker } from '@faker-js/faker/locale/en'
 import { prisma } from './database.mjs'
 import { logger } from './logger.mjs'
 
+const isDev = process.env.NODE_ENV === 'development'
+// Generate new password in DEVELOPMENT
+
 export const manager = async () => {
   const password = faker.internet.password({ length: 6, memorable: true })
   const data = {
     email: 'admin@gmail.com',
-    username: faker.internet.userName(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
     owner: true,
     password
   }
 
   const exist = await prisma.user.findFirst({ where: { email: data.email } })
-  if (exist) return
-
   data.password = await bcryptjs.hash(password, 10)
 
-  await prisma.user.create({
-    data
-  })
+  if (exist) {
+    isDev &&
+      (await prisma.user.update({
+        data: {
+          password
+        },
+        where: {
+          email: data.email
+        }
+      }))
+  } else {
+    await prisma.user.create({
+      data
+    })
+  }
 
   data.password = password
 
