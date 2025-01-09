578899fc8940a1b8eb934c2720b2456d0b777058
Jhonathan M. <nathan3boss@gmail.com>
2025-01-08 21:04:17 -0300

    fix(web): lint format (#19)

--- apps/web/api/actions/edit-user.action.ts
+++ apps/web/api/actions/edit-user.action.ts
@@ -2,8 +2,8 @@
 
 import { api } from '~/api/server'
 
-import { EditUserFormValues } from '~/lib/schemas/edit-user.schema'
 import { UserWithRole } from '~/api/queries/get-users.query'
+import { EditUserFormValues } from '~/lib/schemas/edit-user.schema'
 
 export const editUserAction = async ({ id, ...payload }: EditUserFormValues) => {
   const { data } = await api.put<UserWithRole>(`/user/update/${id}`, payload)
